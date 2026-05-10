c427a94c71ba85592cc2b4830ea8a3ea93f3635d
Damian Aldair <daldairgp@gmail.com>
2026-05-09 23:57:39 -0400

    Updating test

--- test/easy_ql_test.dart
+++ test/easy_ql_test.dart
@@ -104,10 +104,7 @@ void main() {
               'fk1',
               type: DataType.bigInteger,
               nullConflictClause: ConflictClause.rollback,
-              references: ForeignKeyReference(
-                referencedTable: 'referenced_table',
-                referencedColumn: {'rc1'},
-              ),
+              references: ForeignKeyReference('referenced_table', 'rc1'),
             ),
             const TableColumn(
               'fk2',
